14259e00d763054e45cd8b574ca49d517214059c
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-10-29 11:35:47 +0100

    add background to ui design

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
   entry: [
     'react-dev-utils/webpackHotDevClient',
-    __dirname + '/web/src/index.js'
+    __dirname + '/web/src/index.js',
   ],
   output: {
     path: __dirname + '/web/public',
@@ -14,7 +13,7 @@ module.exports = {
   devServer: {
     contentBase: __dirname + '/web/public',
     hot: true,
-    overlay: true
+    overlay: true,
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
@@ -30,18 +29,18 @@ module.exports = {
         test: /\.html$/,
         use: { loader: 'html-loader' },
       },
-       {
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif|woff|woff2|eot|ttf|otf|svg)$/,
         use: [
           {
-            loader: 'file-loader'
-          }
-        ]
-      }
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
